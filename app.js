e3390a22d77c8cc2cf3aee762780c5792ff6786d
Ramashish <ramashishvishwakarma1@gmail.com>
2024-05-20 20:41:10 +0530

    port changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 import { router } from "./routtes/routes.js";
 import { connectDb, dburl } from "./db/db.js";
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
